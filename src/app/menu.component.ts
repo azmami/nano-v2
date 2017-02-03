f6a30b0ec531ab93d24349274267bc64b52da79b
Mamina Araki <mamina.araki@gmail.com>
2017-02-03 17:09:05 +0900

    Created template and stylesheet for MenuComponent

--- src/app/menu.component.ts
+++ src/app/menu.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
     selector: 'main-menu',
-    template: `
-    <div id="left-pane">
-        {{title}}
-    </div>`,
-    styles: [`
-        #left-pane {
-            width: 100%;
-            height: 100%;
-            margin: 0px;
-            padding: 0px;
-        }
-    `]
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
     private title: string;
-    constructor() {
-        this.title = "menu here..";
+    private assets: FirebaseListObservable<any[]>;
+    
+    private assetsSubscription: any;
+    
+    constructor(private angularFire: AngularFire) {
+        this.angularFire.auth.subscribe(update => {
+            console.log(update);
+            if(update) {     
+                this.assets = this.angularFire.database.list(`/assets/${update.uid}`);
+            } else {
+                if(this.assetsSubscription) {
+                    
+                }
+            }
+        });
     }
 }
